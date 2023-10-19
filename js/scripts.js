d77a13f5457fa2871a2394f6fb4444b91dc019c6
@mary-lev <marylevchenko@gmail.com>
2023-10-19 02:56:31 +0200

    Metadata block

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,8 @@ for (let type of types) {
         queues[key].push(elements[i]);
 
         const listItem = document.createElement('span');
-        listItem.className = 'dropdown-item';
+        // listItem.className = 'dropdown-item';
+        listItem.className = 'dropdown-item dropdown-item-metadata';
         listItem.innerHTML = elements[i].innerHTML;
         listItem.dataset.referenceKey = key;
 
