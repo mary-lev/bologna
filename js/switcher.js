9752298d420a0ab056983798a87c4087a3072091
@mary-lev <marylevchenko@gmail.com>
2023-12-04 02:04:48 +0100

    Fix hippie menu

--- js/switcher.js
+++ js/switcher.js
@@ -1,3 +1,12 @@
 function switchStyle(sheet) {
-    document.getElementById('pagestyle').setAttribute('href', sheet); 
+    document.getElementById('pagestyle').setAttribute('href', sheet);
+    window.scrollTo(0, 0); // Scrolls to the top of the document
+
+    // Get the navbar collapse element
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+
+    // Check if the navbarCollapse has the 'show' class and remove it if present
+    if (navbarCollapse.classList.contains('show')) {
+        navbarCollapse.classList.remove('show');
+    }
 }
